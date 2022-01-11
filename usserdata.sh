d6a24945020479dc3294b26cba2e32ef2a99bdf1
BODA MAHESH KUMAR <93734751+mahi-302@users.noreply.github.com>
2022-01-11 07:55:23 +0530

    Update usserdata.sh

--- usserdata.sh
+++ usserdata.sh
@@ -4,4 +4,4 @@ sudo apt-get upgrade -y
 sudo apt-get install awscli -y
 sudo apt-get install apache2 -y
 sudo chmod -R 777 /var/www/html
-aws s3 cp s3:// /var/www/html/index.html
+aws s3 cp s3://demo-8842/index.html/var/www/html/index.html
