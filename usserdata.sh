2d9f2b430afed6c8d41c513813528b38eb1d277f
BODA MAHESH KUMAR <93734751+mahi-302@users.noreply.github.com>
2022-01-11 07:44:12 +0530

    Create usserdata.sh

---
+++ usserdata.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install awscli -y
+sudo apt-get install apache2 -y
+sudo chmod -R 777 /var/www/html
+aws s3 cp s3:// /var/www/html/index.html
